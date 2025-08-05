0c376391b24a615d49003f6667101dc27d49cd92
LakshayYadav167 <lakshayrao32@gmail.com>
2025-08-05 15:08:00 +0530

    Add app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, TechNova!"
+@app.route(/)
+def hello():
+    return Hello, TechNova!
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == __main__:
+    app.run(host=0.0.0.0, port=5000)
