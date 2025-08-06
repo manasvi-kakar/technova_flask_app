e36ada9c266755767e67a895627250dc7edde11b
LakshayYadav167 <lakshayrao32@gmail.com>
2025-08-06 14:20:16 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAY35PXSXADGVWPBYO"
+  secret_key = "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"
+}
+
+variable "key_name" {
+  default = "my-key.pem"
+}
+
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "key-pair-2.pem"
+    public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.rsa_4096.private_key_pem
+  filename        = var.key_name
+  file_permission = "0400"
+}
+
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_ec2"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-0f918f7e67a3323f0"
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+  user_data              = file("user_data.sh")
+
+  tags = {
+    Name = "TechNova"
+  }
+}
+
+#  Output public IP of the instance
+output "ec2_public_ip" {
+  value = aws_instance.public_instance.public_ip
+}
