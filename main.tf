f6209820a805465971ea34fa31f6257a179c2de5
IshaJain <ishajain982004@gmail.com>
2025-08-07 23:50:25 +0530

    Add main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAY35PXSXADGVWPBYO"
-  secret_key = "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"
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
 }
 
 variable "key_name" {
